619d2fd178c1add65af4fca5903193593a46261b
Sébastien Combéfis <seb478@gmail.com>
2020-12-03 10:36:11 +0100

    Add google analytics plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  siteName: "Christmas Market",
-  siteUrl: "https://www.christmas-market.be",
-  pathPrefix: "/",
+  siteName: 'Christmas Market',
+  siteUrl: 'https://www.christmas-market.be',
+  pathPrefix: '/',
   plugins: [{
-    use: "@gridsome/source-graphql",
+    use: '@gridsome/source-graphql',
     options: {
-      url: "https://api-eu-central-1.graphcms.com/v2/ckfmmu5vh21sb01wmbwn58p0a/master",
-      fieldName: "data",
+      url: 'https://api-eu-central-1.graphcms.com/v2/ckfmmu5vh21sb01wmbwn58p0a/master',
+      fieldName: 'data',
       headers: {
         Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
       },
     },
   }, {
-    use: "gridsome-plugin-i18n",
+    use: 'gridsome-plugin-i18n',
     options: {
       locales: [
         'fr-be',
@@ -29,6 +29,13 @@ module.exports = {
       messages: {
         'fr-be': require('./src/locales/fr-be.json'),
         'nl-be': require('./src/locales/nl-be.json')
+      }
+    }
+  }, {
+    use: 'gridsome-plugin-gtag',
+    options: {
+      config: {
+        id: 'G-YMGVGNWKGK'
       }
     }
   }]
